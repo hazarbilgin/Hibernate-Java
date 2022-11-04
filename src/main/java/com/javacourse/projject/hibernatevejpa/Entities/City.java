3bc1af56b7b701500ca28ae5c405c77b252bb7d6
hazarbilgin <71036388+hazarbilgin@users.noreply.github.com>
2022-11-04 20:11:48 +0300

    Update City.java

--- src/main/java/com/javacourse/projject/hibernatevejpa/Entities/City.java
+++ src/main/java/com/javacourse/projject/hibernatevejpa/Entities/City.java
@@ -1,16 +1,19 @@
 package com.javacourse.projject.hibernatevejpa.Entities;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import org.springframework.beans.factory.BeanCreationException;
 
 
 @Entity
 @Table(name="city")
-public class City {
+public class city {
 	@Id
 	@Column(name="ID")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -18,7 +21,7 @@ private int id;
 	@Column(name="Name")
 private String name;
 
-public City(int id, String name, String countryCode, String district,int population) {	
+public city(int id, String name, String countryCode, String district,int population) {	
 	this.id = id;
 	this.name = name;
 	this.countryCode = countryCode;
@@ -64,7 +67,8 @@ public int getPopulation() {
 public void setPopulation(int population) {
 	this.population = population;
 }
-public City() {
+
+public city() {
 	
 }
 }
