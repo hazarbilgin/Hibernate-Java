61499c0335bf487de74892f0bc2b092d2246e82f
hazarbilgin <71036388+hazarbilgin@users.noreply.github.com>
2022-11-04 20:12:25 +0300

    Update HibernateCityDal.java

--- src/main/java/com/javacourse/projject/hibernatevejpa/DataAccess/HibernateCityDal.java
+++ src/main/java/com/javacourse/projject/hibernatevejpa/DataAccess/HibernateCityDal.java
@@ -6,40 +6,43 @@ import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import com.javacourse.projject.hibernatevejpa.Entities.City;
+import com.javacourse.projject.hibernatevejpa.Entities.city;
+import jakarta.persistence.*;
 import jakarta.persistence.EntityManager;
+import java.sql.*;
 
 @Repository
 public class HibernateCityDal implements ICityDal {
+	
 	private EntityManager entityManager;	
 	
 	@Autowired
-	public HibernateCityDal(EntityManager entityManager) {
-		this.entityManager = entityManager;
+	public HibernateCityDal() {
+
 	}
 
 	@Override
 	@Transactional
-	public List<City> GetAll() {
+	public List<city> GetAll() {
 		Session session=entityManager.unwrap(Session.class);
-		List<City> cities=session.createQuery("from world_x.city",City.class).getResultList();
+		List<city> cities=session.createQuery("from world_x.city",city.class).getResultList();
 		return cities;
 	}
 
 	@Override
-	public void add(City city) {
+	public void add(city City) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void update(City city) {
+	public void update(city City) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void delete(City city) {
+	public void delete(city City) {
 		// TODO Auto-generated method stub
 		
 	}
