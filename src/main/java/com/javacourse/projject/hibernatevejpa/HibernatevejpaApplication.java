978ad93a143e222fd3106b3faf21f9a394fd13f4
hazarbilgin <71036388+hazarbilgin@users.noreply.github.com>
2022-11-04 20:13:38 +0300

    Update HibernatevejpaApplication.java

--- src/main/java/com/javacourse/projject/hibernatevejpa/HibernatevejpaApplication.java
+++ src/main/java/com/javacourse/projject/hibernatevejpa/HibernatevejpaApplication.java
@@ -2,12 +2,20 @@ package com.javacourse.projject.hibernatevejpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import com.javacourse.projject.hibernatevejpa.Entities.city;
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class HibernatevejpaApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HibernatevejpaApplication.class, args);
+		System.out.println("sadasda");
+		
+		
+		
 	}
 
 }
